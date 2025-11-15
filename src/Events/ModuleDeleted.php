a6e1899b7365b042db017bd08553f7166ce8dba4
Hamza Mughales <hamzawemughales@gmail.com>
2025-11-15 23:09:13 +0300

    feat: add module events

---
+++ src/Events/ModuleDeleted.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace NootPro\SubscriptionPlans\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use NootPro\SubscriptionPlans\Models\PlanModule;
+
+class ModuleDeleted
+{
+    use Dispatchable;
+
+    public function __construct(
+        public PlanModule $module
+    ) {}
+}
+
